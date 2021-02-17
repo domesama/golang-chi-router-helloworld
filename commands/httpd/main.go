1c3a7464939a227e5fff7bdd47c7599d3179f5a8
Thethawat Kongyu <thethawat.k@mail.kmutt.ac.th>
2021-02-17 15:38:25 +0700

    [Add] handlers

--- commands/httpd/main.go
+++ commands/httpd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/domesama/golang-chi-router-helloworld/packages/newsfeed"
 	"github.com/go-chi/chi"
 	"log"
 	"net/http"
@@ -13,13 +15,30 @@ func main() {
 
 	router := chi.NewRouter()
 
+	feed := newsfeed.New()
+	feed.Add(newsfeed.Item{"OwO", "Post UwU"})
+
 	router.Get("/", func(w http.ResponseWriter, r *http.Request){
 		_,err := w.Write([]byte("Hello World"))
 		if err != nil{
 			log.Fatal(err)
 		}
 	})
-	
+
+	router.Get("/newsfeed", func(w http.ResponseWriter,req *http.Request){
+		items := feed.GetAll()
+		json.NewEncoder(w).Encode(items)
+	})
+
+	router.Post("/newsfeed", func(w http.ResponseWriter, req *http.Request){
+		request := map[string]string{}
+		json.NewDecoder(req.Body).Decode(&request)
+
+		feed.Add(newsfeed.Item{Title: request["title"], Post: request["title"]})
+
+		w.WriteHeader(200)
+	})
+
 	//http.ListenAndServe(":8080", router)
 	server := &http.Server{
 		Addr:             portNumber,
